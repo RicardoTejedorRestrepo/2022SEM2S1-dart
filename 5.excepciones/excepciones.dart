884d6f226c4a951756153471fcc89310842e6f6f
RicardoTejedorRestrepo <103795653+RicardoTejedorRestrepo@users.noreply.github.com>
2022-09-03 18:09:46 -0500

    Update excepciones.dart

--- 5.excepciones/excepciones.dart
+++ 5.excepciones/excepciones.dart
@@ -1,1 +1,23 @@
-void main() {}
+void main() {
+  try{
+    String elNombre = obtenerNombre("ricardo");
+    print(elNombre);
+  } on FormatException catch(e){
+    print("se presento una excepcion FormatException = $e");
+  } on Exception catch(e){
+    print("se presento una excepcion Exception = $e");
+  }catch(e){
+    print("se presento una excepcion general = $e");
+  }
+}
+
+
+String obtenerNombre(String nombre){
+  if(nombre == 'ricardo'){
+    throw FormatException("Este nombre no esta permitido");
+  }
+  if(nombre == 'tejedor'){
+    throw Exception("Este nombre no esta permitido");
+  }
+  return 'Mi nombre es $nombre';
+}

1ff0834b36f350e9f22b2deaf3adb1e9e0141116
RicardoTejedorRestrepo <103795653+RicardoTejedorRestrepo@users.noreply.github.com>
2022-09-03 16:17:44 -0500

    Update variables.dart

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -1,1 +1,11 @@
-void main() {}
+  var nombre = "Ricardo";
+  String nombre_2 = "Andres";
+  print(nombre);
+  print(nombre_2);
+
+  final String apellido = "Tejedor";
+  const String apellido_2 = "Restrepo";
+  print(apellido);
+  print(apellido_2);
+
+}

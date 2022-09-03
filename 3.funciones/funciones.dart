b81dd0467e9d4cc0c8505f433861d60572b17cac
RicardoTejedorRestrepo <103795653+RicardoTejedorRestrepo@users.noreply.github.com>
2022-09-03 18:09:11 -0500

    Update funciones.dart

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() {
+
+  print(obtenerNombre('Ricardo'));
+  print(obtenerNombreApellidos('Ricardo', 'Tejedor'));
+  print(obtenerNombreApellidosParameter(apellidos: 'Tejedor'));
+
+}
+
+String obtenerNombre(String nombre){
+  return 'Mi nombre es $nombre';
+}
+
+String obtenerNombreApellidos(String nombre, String apellidos){
+  return 'Mi nombre es $nombre y mi apellido es $apellidos';
+}
+
+String obtenerNombreApellidosParameter(
+  {String? nombre, required String apellidos, int edad = 0}){
+  return 'Mi nombre es $nombre y mi apellido es $apellidos';
+}
+
